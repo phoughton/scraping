1c6467720176f254191ec9834fe8971b0b8f8583
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-03-10 10:47:48 +0000

    Reorg of files to remove top level folder

--- medspider/settings.py
+++ medspider/settings.py
@@ -15,13 +15,13 @@ SPIDER_MODULES = ['medspider.spiders']
 NEWSPIDER_MODULE = 'medspider.spiders'
 
 ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
-IMAGES_STORE = '/home/ec2-user/environment/scraping/images/'
+IMAGES_STORE = '/home/ec2-user/environment/scraping/medspider/images/'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'medspider (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) (KHTML, like Gecko) Chrome/62.0.3163.100'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -29,9 +29,9 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 3
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
--- medspider/spiders/medicalSpider.py
+++ medspider/spiders/medicalSpider.py
@@ -2,17 +2,12 @@ import scrapy
 import os
 import urllib.parse
 
-out_file="results1.json"
-if os.path.exists(out_file):
-    os.remove(out_file)
-
 
 class MedicalSpider(scrapy.Spider):
     name = 'medspider'
     start_urls = ["https://www.medicines.org.uk/emc/browse-companies"]
 
     def parse(self, response):
-        # for company_name in response.css('a.key'):
 
         for alphabet_item in response.css("ul.browse>li>a"):
             yield response.follow(alphabet_item, self.parse)
@@ -23,9 +18,9 @@ class MedicalSpider(scrapy.Spider):
     
     def parse_company_details(self, response):
         raw_contact = ' '.join(response.css('div.gfdCompanyDetailsCol ::text').getall())
-        yield {
+        captured_data = {
             'companyName': response.css('h1 ::text').get(), 'companyContactDetails': " ".join(raw_contact.split()),
             "image_urls": [urllib.parse.urljoin(response.url,response.css('img.img-responsive::attr(src)').get() )]
         }
-        
+        yield captured_data
 
